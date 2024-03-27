df7afcf36cad9015107c612e522021ae8526d6ec
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2024-03-27 15:35:56 +0530

    Structured the code

---
+++ EventHandlers.cs
@@ -0,0 +1,256 @@
+﻿using BackEnd;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using Point = BackEnd.Point;
+
+namespace BasicScrawls;
+#region class EventHandler ------------------------------------------------------------------------
+public class EventHandler {
+   #region constructor ----------------------------------------------
+   public EventHandler (MainWindow source) => main = source;
+   MainWindow main;
+   #endregion
+
+   #region Methods --------------------------------------------------
+   public void EBinOpen (RoutedEventArgs e) {
+      OpenFileDialog binOpen = new ();
+      if (binOpen.ShowDialog () == true) {
+         if (!mIsSaved && mShapesList.Count > 0) {
+            MessageBoxResult result = MessageBox.Show ("Do you want to save changes before opening?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Cancel) {
+               e.Handled = true;
+               return;
+            } else if (result == MessageBoxResult.Yes) {
+               if (mSavedFileName == null) SaveNew ();
+               else SaveChanges (mSavedFileName);
+            }
+         }
+         mShapesList.Clear ();
+         mUndoRedo.Clear ();
+         BinaryReader br = new (File.Open (binOpen.FileName, FileMode.Open));
+         int shapeCount = br.ReadInt32 ();
+         for (int i = 0; i < shapeCount; i++) {
+            int num = br.ReadInt32 ();
+            switch (num) {
+               case 1: mShapes = new Scrawls (); break;
+               case 2: mShapes = new Line (); break;
+               case 3: mShapes = new Rectangle (); break;
+               case 4: mShapes = new Circle (); break;
+               case 5: mShapes = new ConnectedLine (); break;
+            }
+            mShapes.Load (br);
+            mShapesList.Add (mShapes);
+         }
+         main.InvalidateVisual ();
+         mSavedFileName = binOpen.FileName;
+         mIsSaved = true;
+         main.Title = $"{Path.GetFileNameWithoutExtension (mSavedFileName)} - Scribble Pad";
+      }
+   }
+
+   public void EBinSave () {
+      if (mSavedFileName == null) SaveNew ();
+      else SaveChanges (mSavedFileName);
+      main.Title = $"{Path.GetFileNameWithoutExtension (mSavedFileName)} - Scribble Pad";
+   }
+
+   public void EClear () {
+      mShapes = null;
+      mShapesList.Clear ();
+      mUndoRedo.Clear ();
+      main.InvalidateVisual ();
+   }
+
+   public void EConLine () {
+      mShapesList.Add (mShapes);
+      main.StatusBar.Visibility = Visibility.Collapsed;
+      main.InvalidateVisual ();
+      mShapes = new ConnectedLine ();
+   }
+
+   public void EDockCheck (RoutedEventArgs e) {
+      FrameworkElement fwe = e.Source as FrameworkElement;
+      mSelectedItem = fwe as ToggleButton;
+      mSelectedItem.Click += ToggleButton_Click;
+      e.Handled = true;
+   }
+
+   public void ENew (RoutedEventArgs e) {
+      if (!mIsSaved && mShapesList.Count > 0) {
+         MessageBoxResult result = MessageBox.Show ("Do you want to save changes before switching to a new file?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
+         if (result == MessageBoxResult.Cancel) {
+            e.Handled = true;
+            return;
+         } else if (result == MessageBoxResult.Yes) {
+            if (mSavedFileName == null) SaveNew ();
+            else SaveChanges (mSavedFileName);
+         }
+      }
+      EClear ();
+      main.Title = "Untitled - Scribble Pad";
+   }
+
+   public void EMouseDown (MouseButtonEventArgs e) {
+      if (e.LeftButton == MouseButtonState.Pressed) {
+         Point pt;
+         pt.X = e.GetPosition (main).X;
+         pt.Y = e.GetPosition (main).Y;
+         switch (mSelectedItem.Name) {
+            case "scrawl":
+               mShapes = new Scrawls ();
+               break;
+            case "line":
+               mShapes = new Line ();
+               break;
+            case "rect":
+               mShapes = new Rectangle ();
+               break;
+            case "circle":
+               mShapes = new Circle ();
+               break;
+            case "conLine":
+               main.StatusBar.Visibility = Visibility.Visible;
+               break;
+         }
+         mShapes.pointList.Add (pt);
+         mShapes.pointList.Add (pt);
+         mUndoRedo.Clear ();
+         main.redo.IsEnabled = false;
+         mIsSaved = false;
+      }
+   }
+
+   public void EMouseMove (MouseEventArgs e) {
+      if (e.LeftButton == MouseButtonState.Pressed) {
+         Point pt;
+         pt.X = e.GetPosition (main).X;
+         pt.Y = e.GetPosition (main).Y;
+         mShapes.UpdateEndPoint (pt);
+         main.InvalidateVisual ();
+      }
+   }
+
+   public void EMouseUp (MouseButtonEventArgs e) {
+      if (e.LeftButton == MouseButtonState.Released) {
+         Point pt;
+         pt.X = e.GetPosition (main).X;
+         pt.Y = e.GetPosition (main).Y;
+         mShapes.UpdateEndPoint (pt);
+         if (mShapes is not ConnectedLine) mShapesList.Add (mShapes);
+         main.InvalidateVisual ();
+         main.undo.IsEnabled = true;
+      }
+   }
+
+   public void ERedo () {
+      if (mUndoRedo.Count > 0) {
+         mShapesList.Add (mUndoRedo.Pop ());
+         main.InvalidateVisual ();
+      }
+   }
+
+   public void ERender (DrawingContext dc) {
+      foreach (var shape in mShapesList) if (shape != null) Draw (dc, shape);
+      if (mShapes != null) Draw (dc, mShapes);
+   }
+
+   public void EUndo () {
+      mShapes = null;
+      if (mShapesList.Count > 0 && !mIsSaved) {
+         mUndoRedo.Push (mShapesList.Last ());
+         mShapesList.Remove (mShapesList.Last ());
+         main.InvalidateVisual ();
+      }
+      main.redo.IsEnabled = true;
+   }
+   #endregion
+
+   #region Implementation -------------------------------------------
+   //To draw the respective shapes
+   private void Draw (DrawingContext dc, Shapes shape) {
+      System.Windows.Point start, end;
+      if (shape is Rectangle or Circle) {
+         start.X = shape.pointList[0].X; start.Y = shape.pointList[0].Y;
+         end.X = shape.pointList[^1].X; end.Y = shape.pointList[^1].Y;
+         switch (shape) {
+            case Rectangle: dc.DrawRectangle (null, mPen, new (start, end)); break;
+            case Circle:
+               double x = end.X - start.X;
+               double y = end.Y - start.Y;
+               double radius = Math.Sqrt ((x * x) + (y * y));
+               dc.DrawEllipse (null, mPen, start, radius, radius);
+               break;
+         }
+      } else
+         for (int i = 0; i < shape.pointList.Count - 1; i++) {
+            start.X = shape.pointList[i].X; start.Y = shape.pointList[i].Y;
+            end.X = shape.pointList[i + 1].X; end.Y = shape.pointList[i + 1].Y;
+            dc.DrawLine (mPen, start, end);
+         }
+   }
+
+   //Shows messagebox when attempting to close the window without saving
+   public void MainWindow_Closing (object sender, CancelEventArgs e) {
+      if (!mIsSaved && mShapesList.Count > 0) {
+         MessageBoxResult result = MessageBox.Show ("Do you want to save changes before closing?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
+         if (result == MessageBoxResult.Cancel) e.Cancel = true;
+         else if (result == MessageBoxResult.Yes) {
+            if (mSavedFileName == null) SaveNew ();
+            else SaveChanges (mSavedFileName);
+         }
+      }
+   }
+
+   //Saves the changes done on a saved file
+   private void SaveChanges (string fileName) {
+      using (BinaryWriter bw = new (File.Open (fileName, FileMode.Create))) {
+         bw.Write (mShapesList.Count);
+         foreach (var shape in mShapesList) shape.Save (bw);
+      }
+      mIsSaved = true;
+   }
+
+   //Saves a new file i.e.,saves the file for the first time
+   private void SaveNew () {
+      SaveFileDialog binSave = new ();
+      binSave.FileName = "Untitled";
+      binSave.DefaultExt = ".txt";
+      binSave.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+      if (binSave.ShowDialog () == true) {
+         mSavedFileName = binSave.FileName;
+         SaveChanges (binSave.FileName);
+      }
+   }
+
+   //Ensures only one toggle button is on at a time
+   private void ToggleButton_Click (object sender, RoutedEventArgs e) {
+      var clickedToggleButton = sender as ToggleButton;
+      if (clickedToggleButton.IsChecked == true) {
+         foreach (var control in main.Dock.Children)
+            if (control is ToggleButton toggleButton)
+               toggleButton.IsChecked = (toggleButton == clickedToggleButton);
+      } else clickedToggleButton.IsChecked = true;
+   }
+   #endregion
+
+   #region Private --------------------------------------------------
+   Pen mPen = new (Brushes.White, 1);
+   Shapes mShapes = null;
+   List<Shapes> mShapesList = new ();
+   Stack<Shapes> mUndoRedo = new ();
+   ToggleButton mSelectedItem = null;
+   bool mIsSaved = false;
+   string mSavedFileName = null;
+   #endregion
+}
+#endregion
+
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,294 +1,58 @@
-﻿using BasicScrawls;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Ink;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Input.StylusPlugIns;
 using System.Windows.Media;
-using System.Windows.Media.Converters;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
 namespace BasicScrawls {
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
    public partial class MainWindow : Window {
+      #region constructor -------------------------------------------
       public MainWindow () {
+         mEvents = new EventHandler (this);
          InitializeComponent ();
-         Closing += MainWindow_Closing;
-         mToolTip.Content = "To stop (or) start a new one, click on the connected line tool again.";
+         Closing += mEvents.MainWindow_Closing;
       }
+      EventHandler mEvents;
+      #endregion
 
-      Pen mPen = new (Brushes.White, 1);
-      Shapes mShapes = null;
-      ConnectedLine mConnectedLine = null;
-      List<Shapes> mShapesList = new ();
-      Stack<Shapes> mUndoRedo = new ();
-      ToggleButton mSelectedItem = null;
-      ToolTip mToolTip = new ();
-      bool IsSaved = false;
-      string mSavedFileName = null;
-
+      #region EventHandlers -----------------------------------------
       //Opens an existing file
-      private void BinOpen_Click (object sender, RoutedEventArgs e) {
-         OpenFileDialog binOpen = new ();
-         if (binOpen.ShowDialog () == true) {
-            if (!IsSaved && mShapesList.Count > 0) {
-               MessageBoxResult result = MessageBox.Show ("Do you want to save changes before opening?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
-               if (result == MessageBoxResult.Cancel) {
-                  e.Handled = true;
-                  return;
-               } else if (result == MessageBoxResult.Yes) {
-                  if (mSavedFileName == null) SaveNew ();
-                  else SaveChanges (mSavedFileName);
-               }
-            }
-            mShapesList.Clear ();
-            mUndoRedo.Clear ();
-            BinaryReader br = new (File.Open (binOpen.FileName, FileMode.Open));
-            int shapeCount = br.ReadInt32 ();
-            for (int i = 0; i < shapeCount; i++) {
-               int num = br.ReadInt32 ();
-               switch (num) {
-                  case 1: mShapes = new Scrawls (); break;
-                  case 2: mShapes = new Line (); break;
-                  case 3: mShapes = new Rect (); break;
-                  case 4: mShapes = new Circle (); break;
-                  case 5: mShapes = new ConnectedLine (); break;
-               }
-               int pointsCount = br.ReadInt32 ();
-               for (int j = 0; j < pointsCount; j++) {
-                  double x = br.ReadDouble ();
-                  double y = br.ReadDouble ();
-                  mShapes.PointList.Add (new Point (x, y));
-               }
-               mShapesList.Add (mShapes);
-            }
-            InvalidateVisual ();
-            mSavedFileName = binOpen.FileName;
-            IsSaved = true;
-            Title = $"{System.IO.Path.GetFileNameWithoutExtension (mSavedFileName)} - Scribble Pad";
-         }
-      }
+      private void OnBinOpen_Click (object sender, RoutedEventArgs e) => mEvents.EBinOpen (e);
 
       //Saves the file
-      private void BinSave_Click (object sender, RoutedEventArgs e) {
-         if (mSavedFileName == null) SaveNew ();
-         else SaveChanges (mSavedFileName);
-         Title = $"{Path.GetFileNameWithoutExtension (mSavedFileName)} - Scribble Pad";
-      }
+      private void OnBinSave_Click (object sender, RoutedEventArgs e) => mEvents.EBinSave ();
 
       //Clears the window
-      private void Clear_Click (object sender, RoutedEventArgs e) {
-         mShapesList.Clear ();
-         mUndoRedo.Clear ();
-         InvalidateVisual ();
-      }
+      private void OnClear_Click (object sender, RoutedEventArgs e) => mEvents.EClear ();
 
       //Click event for connected line icon
-      private void ConLine_Click (object sender, RoutedEventArgs e) {
-         mShapesList.Add (mConnectedLine);
-         mToolTip.IsOpen = false;
-         mConnectedLine = new ();
-      }
+      private void OnConLine_Click (object sender, RoutedEventArgs e) => mEvents.EConLine ();
 
       //Click event for all toggle buttons on the dock panel
-      private void DockPanel_Checked (object sender, RoutedEventArgs e) {
-         FrameworkElement fwe = e.Source as FrameworkElement;
-         mSelectedItem = fwe as ToggleButton;
-         mSelectedItem.Click += ToggleButton_Click;
-         e.Handled = true;
-      }
+      private void DockPanel_Checked (object sender, RoutedEventArgs e) => mEvents.EDockCheck (e);
 
-      //Shows messagebox when attempting to close the window without saving
-      private void MainWindow_Closing (object sender, CancelEventArgs e) {
-         if (!IsSaved && mShapesList.Count > 0) {
-            MessageBoxResult result = MessageBox.Show ("Do you want to save changes before closing?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
-            if (result == MessageBoxResult.Cancel) e.Cancel = true;
-            else if (result == MessageBoxResult.Yes) {
-               if (mSavedFileName == null) SaveNew ();
-               else SaveChanges (mSavedFileName);
-            }
-         }
-      }
+      //Creates a new file
+      private void OnNew_Click (object sender, RoutedEventArgs e) => mEvents.ENew (e);
 
-      //Opens a new file
-      private void New_Click (object sender, RoutedEventArgs e) {
-         if (!IsSaved && mShapesList.Count > 0) {
-            MessageBoxResult result = MessageBox.Show ("Do you want to save changes before switching to a new file?", "Unsaved Changes", MessageBoxButton.YesNoCancel);
-            if (result == MessageBoxResult.Cancel) {
-               e.Handled = true;
-               return;
-            } else if (result == MessageBoxResult.Yes) {
-               if (mSavedFileName == null) SaveNew ();
-               else SaveChanges (mSavedFileName);
-            }
-         }
-         Clear_Click (sender, e);
-         Title = "Untitled - Scribble Pad";
-      }
+      //Performs redo action
+      private void OnRedo_Click (object sender, RoutedEventArgs e) => mEvents.ERedo ();
 
-      protected override void OnMouseLeftButtonDown (MouseButtonEventArgs e) {
-         if (e.LeftButton == MouseButtonState.Pressed) {
-            Point pt = e.GetPosition (this);
-            switch (mSelectedItem.Name) {
-               case "scrawl":
-                  mShapes = new Scrawls ();
-                  mShapes.PointList.Add (pt);
-                  mShapesList.Add (mShapes);
-                  break;
-               case "line":
-                  mShapes = new Line ();
-                  mShapes.PointList.Add (pt);
-                  mShapes.PointList.Add (pt);
-                  mShapesList.Add (mShapes);
-                  break;
-               case "rect":
-                  mShapes = new Rect ();
-                  mShapes.PointList.Add (pt);
-                  mShapes.PointList.Add (pt);
-                  mShapesList.Add (mShapes);
-                  break;
-               case "circle":
-                  mShapes = new Circle ();
-                  mShapes.PointList.Add (pt);
-                  mShapes.PointList.Add (pt);
-                  mShapesList.Add (mShapes);
-                  break;
-               case "conLine":
-                  mShapes = mConnectedLine;
-                  mShapes.PointList.Add (pt);
-                  mShapes.PointList.Add (pt);
-                  mToolTip.IsOpen = true;
-                  break;
-            }
-            mUndoRedo.Clear ();
-            IsSaved = false;
-         }
-      }
+      //Performs undo action
+      private void OnUndo_Click (object sender, RoutedEventArgs e) => mEvents.EUndo ();
+      #endregion
 
-      protected override void OnMouseLeftButtonUp (MouseButtonEventArgs e) {
-         if (e.LeftButton == MouseButtonState.Released) {
-            Point pt = e.GetPosition (this);
-            if (mShapes is Scrawls) mShapes.PointList.Add (pt);
-            else mShapes.PointList[^1] = pt;
-            mShapesList[^1] = mShapes;
-            InvalidateVisual ();
-         }
-      }
+      #region MouseEvents -------------------------------------------
+      protected override void OnMouseLeftButtonDown (MouseButtonEventArgs e) => mEvents.EMouseDown (e);
 
-      protected override void OnMouseMove (MouseEventArgs e) {
-         if (e.LeftButton == MouseButtonState.Pressed) {
-            Point pt = e.GetPosition (this);
-            if (mShapes is Scrawls) mShapes.PointList.Add (pt);
-            else mShapes.PointList[^1] = pt;
-            mShapesList[^1] = mShapes;
-            InvalidateVisual ();
-         }
-      }
+      protected override void OnMouseLeftButtonUp (MouseButtonEventArgs e) => mEvents.EMouseUp (e);
+
+      protected override void OnMouseMove (MouseEventArgs e) => mEvents.EMouseMove (e);
 
       protected override void OnRender (DrawingContext dc) {
          base.OnRender (dc);
-         foreach (var shape in mShapesList) {
-            switch (shape) {
-               case Scrawls scrawl:
-                  for (int i = 0; i < scrawl.PointList.Count - 1; i++) dc.DrawLine (mPen, scrawl.PointList[i], scrawl.PointList[i + 1]);
-                  break;
-               case Line line:
-                  dc.DrawLine (mPen, line.PointList[0], line.PointList[^1]);
-                  break;
-               case Rect rect:
-                  Point pt1 = rect.PointList[0];
-                  Point pt2 = rect.PointList[1];
-                  dc.DrawRectangle (null, mPen, new (pt1, pt2));
-                  break;
-               case Circle circle:
-                  Point start = circle.PointList[0];
-                  Point end = circle.PointList[^1];
-                  double x = end.X - start.X;
-                  double y = end.Y - start.Y;
-                  double radius = Math.Sqrt ((x * x) + (y * y));
-                  dc.DrawEllipse (null, mPen, start, radius, radius);
-                  break;
-               case ConnectedLine conLine:
-                  for (int i = 0; i < conLine.PointList.Count - 1; i++) dc.DrawLine (mPen, conLine.PointList[i], conLine.PointList[i + 1]);
-                  break;
-            }
-         }
+         mEvents.ERender (dc);
       }
-
-      //Performs redo action
-      private void Redo_Click (object sender, RoutedEventArgs e) {
-         if (mUndoRedo.Count > 0) {
-            mShapesList.Add (mUndoRedo.Pop ());
-            InvalidateVisual ();
-         }
-      }
-
-      //Saves the changes done on a saved file
-      private void SaveChanges (string fileName) {
-         using (BinaryWriter bw = new (File.Open (fileName, FileMode.Create))) {
-            bw.Write (mShapesList.Count);
-            foreach (var shape in mShapesList) {
-               switch (shape) {
-                  case Scrawls: bw.Write (1); break;
-                  case Line: bw.Write (2); break;
-                  case Rect: bw.Write (3); break;
-                  case Circle: bw.Write (4); break;
-                  case ConnectedLine: bw.Write (5); break;
-               }
-               bw.Write (shape.PointList.Count);
-               foreach (Point pt in shape.PointList) {
-                  bw.Write (pt.X);
-                  bw.Write (pt.Y);
-               }
-            }
-         }
-         IsSaved = true;
-      }
-
-      //Saves a new file i.e.,saves the file for the first time
-      private void SaveNew () {
-         SaveFileDialog binSave = new ();
-         binSave.FileName = "Untitled";
-         binSave.DefaultExt = ".txt";
-         binSave.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-         if (binSave.ShowDialog () == true) {
-            mSavedFileName = binSave.FileName;
-            SaveChanges (binSave.FileName);
-         }
-      }
-
-      //Ensures only one toggle button is on at a time
-      private void ToggleButton_Click (object sender, RoutedEventArgs e) {
-         var clickedToggleButton = sender as ToggleButton;
-         if (clickedToggleButton.IsChecked == true) {
-            foreach (var control in Dock.Children)
-               if (control is ToggleButton toggleButton)
-                  toggleButton.IsChecked = (toggleButton == clickedToggleButton);
-         } else clickedToggleButton.IsChecked = true;
-      }
-
-      //Performs undo action
-      private void Undo_Click (object sender, RoutedEventArgs e) {
-         if (mShapesList.Count > 0) {
-            mUndoRedo.Push (mShapesList.Last ());
-            mShapesList.RemoveAt (mShapesList.Count - 1);
-            InvalidateVisual ();
-         }
-      }
+      #endregion
    }
 }
